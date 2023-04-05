ae619dab2e728194ab2b104b786d4d3c069d1a45
jessikea <jessikea@jtran.git>
2023-04-04 20:58:37 -0400

    "js"

--- assets/script.js
+++ assets/script.js
@@ -1,123 +1,111 @@
-$(function () {
-  var city = $("#city-name");
+const apiKey = "77ac543282c79533c2796093c3f6e43e";
 
-  var cityTemp = $("#temp");
-  var cityWind = $("#wind");
-  var cityHumidity = $("#humidity");
+// Initialize variables
+let citySearch;
+const cityElements = {
+  city: document.querySelector("#city-name"),
+  temp: document.querySelector("#temp"),
+  wind: document.querySelector("#wind"),
+  humidity: document.querySelector("#humidity"),
+  icon: document.querySelector("#icon"),
+};
+const forecastDays = document.querySelectorAll(".forecast");
+const searchButton = document.querySelector("#search-btn");
+const historyList = document.querySelector("#previously-searched");
+let cityHistory = [];
 
-  var forecastDay = $(".forecast");
-  var boxIcon = $("#icon");
+// Initialize the app
+function init() {
+  const storedCities = JSON.parse(localStorage.getItem("City"));
+  cityHistory = storedCities || [];
+  renderHistory();
+}
 
-  var citySearch;
-  var cityHistory = [];
+// Render the search history
+function renderHistory() {
+  historyList.innerHTML = "";
+  cityHistory.forEach((city) => {
+    const button = document.createElement("button");
+    button.textContent = city;
+    button.setAttribute("data-city", city);
+    button.classList.add("city-history-btn");
+    historyList.appendChild(button);
+  });
+}
 
-  init();
-
-  function init() {
-    var storedCities = JSON.parse(localStorage.getItem("City"));
-    if (!storedCities) {
-      cityHistory = [];
-    } 
-    else {
-      cityHistory = storedCities;
-    }
+// Save search history to local storage
+function saveHistory(searchedCity) {
+  const storedCities = JSON.parse(localStorage.getItem("City")) || [];
+  if (!storedCities.includes(searchedCity)) {
+    storedCities.push(searchedCity);
+    localStorage.setItem("City", JSON.stringify(storedCities));
     renderHistory();
   }
+}
 
-  function renderHistory() {
-    $("#previously-searched").empty();
-    for (var i = 0; i < cityHistory.length; i++) {
-      var btn = $("<button>");
-      btn.text(cityHistory[i]);
-      btn.attr("data-city", cityHistory[i]);
-      btn.addClass("city-history-btn");
-      $("#previously-searched").append(btn);
+// Search for weather data
+async function searchWeather() {
+  citySearch = document.querySelector("#search-city").value.trim();
+  const cityDataUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&cnt=40&units=imperial&appid=77ac543282c79533c2796093c3f6e43e`;
+
+  try {
+    const response = await fetch(cityDataUrl);
+    const data = await response.json();
+
+    const searchedCity = data.city.name;
+    saveHistory(searchedCity);
+
+    // display current weather
+    await getCurrentWeather(citySearch);
+
+    // display 5-day forecast
+    let i = 0;
+    for (let j = 0; j < 5; j++) {
+      const forecast = forecastDays[j];
+      forecast.querySelector(".date").textContent = dayjs(data.list[i].dt_txt.split(" ")[0]).format("MM/DD/YYYY");
+      forecast.querySelector(".icon").setAttribute("src", `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`);
+      forecast.querySelector(".temp").textContent = `Temp: ${data.list[i].main.temp}F`;
+      forecast.querySelector(".wind").textContent = `Wind: ${data.list[i].wind.speed}`;
+      forecast.querySelector(".humidity").textContent = `Humidity: ${data.list[i].main.humidity}%`;
+      i += 8;
     }
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  function savehistory(searchedName) {
-    var cityHistory = JSON.parse(localStorage.getItem("City")) || [];
-    if (!cityHistory.find((city) => city === searchedName)) {
-      cityHistory.push(searchedName);
-      localStorage.setItem("City", JSON.stringify(cityHistory));
-      renderHistory();
-    }
+// Get current weather data
+async function getCurrentWeather(city) {
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=77ac543282c79533c2796093c3f6e43e`;
+
+  try {
+    const response = await fetch(currentWeatherUrl);
+    const data = await response.json();
+
+    cityElements.city.textContent = `${data.name} (${dayjs().format("MM/DD/YYYY")})`;
+    cityElements.icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    cityElements.temp.textContent = `Temp: ${data.main.temp}F`;
+    cityElements.wind.textContent = `Wind Spd: ${data.wind.speed}`;
+    cityElements.humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  function searchWeather() {
-    citySearch = $("#search-city").val().trim();
-    getWeather();
+
+// Search for weather data using a previously searched city
+function searchHistory(event)
+{
+  if (event.target.classList.contains("city-history-btn")) {
+  const searchedCity = event.target.getAttribute("data-city");
+  document.querySelector("#search-city").value = searchedCity;
+  searchWeather();
   }
-
-  function getWeather() {
-    console.log(citySearch);
-    var cityData = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&cnt=40&units=imperial&appid=745a69533ad8b9cebd150df776c4e1ea`;
-
-    fetch(cityData)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        var searchedName = data.city.name;
-        savehistory(searchedName);
-
-        let i = 0;
-        console.log(forecastDay);
-        for (let j = 1; j < 6; j++) {
-
-          console.log(j, i);
-          console.log(data.list[i]);
-          $(`#f${j}`)
-            .children(".date")
-            .text(
-              dayjs(data.list[i].dt_txt.split(" ")[0]).format("MM/DD/YYYY")
-            );
-          $(`#f${j}`)
-            .children(".icon")
-            .attr(
-              "src",
-              `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`
-            );
-          $(`#f${j}`)
-            .children(".temp")
-            .text("Temp: " + data.list[i].main.temp + "F");
-          $(`#f${j}`)
-            .children(".wind")
-            .text("Wind: " + data.list[i].wind.speed);
-          $(`#f${j}`)
-            .children(".humidity")
-            .text("Humidity: " + data.list[i].main.humidity + "%");
-          i += 8;
-        }
-      });
-    curWeather(citySearch);
   }
-
-  function curWeather(citySearch) {
-    var rnWeather = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=745a69533ad8b9cebd150df776c4e1ea`;
-    fetch(rnWeather)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        city.text(data.name + " (" + dayjs().format("MM/DD/YYYY") + ")");
-        boxIcon.attr(
-          "src",
-          `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        );
-        cityTemp.text("Temp: " + data.main.temp + "F");
-        cityWind.text("Wind Spd: " + data.wind.speed);
-        cityHumidity.text("Humidity: " + data.main.humidity + "%");
-      });
-  }
-
-  function buttonSearch(event) {
-    console.log(event.target);
-    citySearch = $(event.target).attr("data-city");
-    getWeather();
-  }
-
-  $("#search-btn").on("click", searchWeather);
-  $("#previously-searched").on("click", buttonSearch);
-});
+  
+  // Event listeners
+  searchButton.addEventListener("click", searchWeather);
+  historyList.addEventListener("click", searchHistory);
+  
+  // Initialize the app on page load
+  init();
